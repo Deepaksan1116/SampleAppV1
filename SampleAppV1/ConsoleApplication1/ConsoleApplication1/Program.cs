5a2a45129361ee9cad39e12a861cf8df7b956554
Deepaksan1116 <deepakkrct@gmail.com>
2023-01-05 20:45:31 +0530

    project completed

--- SampleAppV1/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ SampleAppV1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine(Add_two_numbers(5,10));
+            Console.WriteLine(Subtract_two_numbers(15,5));
         }
         #region Add two numbers
         public static int Add_two_numbers(int number1,int number2)
@@ -20,7 +21,10 @@ namespace ConsoleApplication1
         #endregion
 
         #region Subtract two numbers
-
+        public static int Subtract_two_numbers(int number1,int number2)
+        {
+            return number1 - number2;
+        }
         #endregion
     }
 }
